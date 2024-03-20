2c12a40ecc56c8221b4c7c29cc895377bd8351cb
OscarDelcid <oscarjoeldelcidrevolorio2004@gmail.com>
2024-03-20 15:53:49 -0600

    hola
    hola

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './heroes/list/list.component';
+import { CountereModule } from './counter/components/counter/counter.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeroComponent,
+    ListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    CountereModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
